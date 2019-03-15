42b7733bfca55ab7854268fcd501aa812ab601d5
EdgarJHJ <edgarjhj@ehuaranga.edhuar>
2019-03-15 11:27:38 -0500

    maps

--- resources/views/location/index.blade.php
+++ resources/views/location/index.blade.php
@@ -3,9 +3,8 @@
   <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
-    <title>Rectangles</title>
+    <title>Signum</title>
     <style>
-
       #map {
         height: 100%;
       }
@@ -37,11 +36,41 @@
             west: -116.251
           }
         });
+
+        loadSignals();
       }
     </script>
     <script type="text/javascript">
-      var locations = {!!json_encode($locations)!!};
-      console.log(locations);
+      function loadSignals(){
+        var locations = {!!json_encode($locations)!!};
+        //console.log(locations);
+
+        for(int i=0; i<locations.length; i++){
+          console.log(locations[i].accuracy)
+        }
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 11,
+          center: {lat: 33.678, lng: -116.243},
+          mapTypeId: 'terrain'
+        });
+
+        var rectangle = new google.maps.Rectangle({
+          strokeColor: '#FF0000',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: '#FF0000',
+          fillOpacity: 0.35,
+          map: map,
+          bounds: {
+            north: 33.685,
+            south: 33.671,
+            east: -116.234,
+            west: -116.251
+          }
+        });
+      }
+
     </script>
   </head>
   <body>
