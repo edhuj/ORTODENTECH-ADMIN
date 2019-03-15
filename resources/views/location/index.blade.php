99c09222e064f314d9aae96b3cdf5d512201774d
EdgarJHJ <edgarjhj@ehuaranga.edhuar>
2019-03-15 11:41:06 -0500

    maps

--- resources/views/location/index.blade.php
+++ resources/views/location/index.blade.php
@@ -15,12 +15,36 @@
       }
     </style>
 
+    <script>
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 11,
+          center: {lat: 33.678, lng: -116.243},
+          mapTypeId: 'terrain'
+        });
+
+        var rectangle = new google.maps.Rectangle({
+          strokeColor: '#FF0000',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: '#FF0000',
+          fillOpacity: 0.35,
+          map: map,
+          bounds: {
+            north: 33.685,
+            south: 33.671,
+            east: -116.234,
+            west: -116.251
+          }
+        });
+
+        loadSignals();
+      }
+    </script>
+
   </head>
   <body>
     <div id="map"></div>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCGAOvDgtDnuRnLbShwZpEGVZTRPZNINIQ&callback=initMap">
-    </script>
     <script type="text/javascript">
       function loadSignals(){
         var locations = {!!json_encode($locations)!!};
@@ -51,34 +75,9 @@
           }
         });
       }
-
     </script>
-    <script>
-      function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 11,
-          center: {lat: 33.678, lng: -116.243},
-          mapTypeId: 'terrain'
-        });
-
-        var rectangle = new google.maps.Rectangle({
-          strokeColor: '#FF0000',
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: '#FF0000',
-          fillOpacity: 0.35,
-          map: map,
-          bounds: {
-            north: 33.685,
-            south: 33.671,
-            east: -116.234,
-            west: -116.251
-          }
-        });
-
-        loadSignals();
-
-      }
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCGAOvDgtDnuRnLbShwZpEGVZTRPZNINIQ&callback=initMap">
     </script>
   </body>
 </html>
