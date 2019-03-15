f912957c782c4c7aa93a4454d06edef840b4b026
EdgarJHJ <edgarjhj@ehuaranga.edhuar>
2019-03-15 12:04:06 -0500

    maps

--- resources/views/location/index.blade.php
+++ resources/views/location/index.blade.php
@@ -29,23 +29,22 @@
         console.log(signalLocations.length);
         for (i = 0; i < signalLocations.length; i++) {
           console.log("Hello "+signalLocations[i].accuracy);
+          new google.maps.Rectangle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: map,
+            bounds: {
+              south: parseFloat(signalLocations[i].latitude)+0.001,
+              north: parseFloat(signalLocations[i].longitude)+0.001,
+              east: parseFloat(signalLocations[i].latitude)-0.001,
+              west: parseFloat(signalLocations[i].longitude)+0.001
+            }
+          });
         }
 
-
-        var rectangle = new google.maps.Rectangle({
-          strokeColor: '#FF0000',
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: '#FF0000',
-          fillOpacity: 0.35,
-          map: map,
-          bounds: {
-            north: 33.685,
-            south: 33.671,
-            east: -116.234,
-            west: -116.251
-          }
-        });
       }
     </script>
 
