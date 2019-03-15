9e080f12dd5479bcd4217dadb515ff8914338908
EdgarJHJ <edgarjhj@ehuaranga.edhuar>
2019-03-15 11:43:45 -0500

    maps

--- resources/views/location/index.blade.php
+++ resources/views/location/index.blade.php
@@ -17,6 +17,10 @@
 
     <script>
       function initMap() {
+
+        var locations = {!!json_encode($locations)!!};
+
+        console.log(locations.lenth);
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 11,
           center: {lat: 33.678, lng: -116.243},
@@ -37,45 +41,12 @@
             west: -116.251
           }
         });
-
-        loadSignals();
       }
     </script>
 
   </head>
   <body>
     <div id="map"></div>
-    <script type="text/javascript">
-      function loadSignals(){
-        var locations = {!!json_encode($locations)!!};
-        //console.log(locations);
-
-        for(int i=0; i<locations.length; i++){
-          console.log(locations[i].accuracy)
-        }
-
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 11,
-          center: {lat: 33.678, lng: -116.243},
-          mapTypeId: 'terrain'
-        });
-
-        var rectangle = new google.maps.Rectangle({
-          strokeColor: '#FF0000',
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: '#FF0000',
-          fillOpacity: 0.35,
-          map: map,
-          bounds: {
-            north: 33.685,
-            south: 33.671,
-            east: -116.234,
-            west: -116.251
-          }
-        });
-      }
-    </script>
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCGAOvDgtDnuRnLbShwZpEGVZTRPZNINIQ&callback=initMap">
     </script>
