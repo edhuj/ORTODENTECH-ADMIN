018da5ba4c0438be94e649f8ab21a350d96dbc16
EdgarJHJ <edgarjhj@ehuaranga.edhuar>
2019-03-15 11:34:36 -0500

    maps

--- resources/views/location/index.blade.php
+++ resources/views/location/index.blade.php
@@ -14,33 +14,6 @@
         padding: 0;
       }
     </style>
-    <script>
-      function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 11,
-          center: {lat: 33.678, lng: -116.243},
-          mapTypeId: 'terrain'
-        });
-
-        var rectangle = new google.maps.Rectangle({
-          strokeColor: '#FF0000',
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: '#FF0000',
-          fillOpacity: 0.35,
-          map: map,
-          bounds: {
-            north: 33.685,
-            south: 33.671,
-            east: -116.234,
-            west: -116.251
-          }
-        });
-
-        loadSignals();
-
-      }
-    </script>
     <script type="text/javascript">
       function loadSignals(){
         var locations = {!!json_encode($locations)!!};
@@ -73,6 +46,34 @@
       }
 
     </script>
+    <script>
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 11,
+          center: {lat: 33.678, lng: -116.243},
+          mapTypeId: 'terrain'
+        });
+
+        var rectangle = new google.maps.Rectangle({
+          strokeColor: '#FF0000',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: '#FF0000',
+          fillOpacity: 0.35,
+          map: map,
+          bounds: {
+            north: 33.685,
+            south: 33.671,
+            east: -116.234,
+            west: -116.251
+          }
+        });
+
+        loadSignals();
+
+      }
+    </script>
+
   </head>
   <body>
     <div id="map"></div>
