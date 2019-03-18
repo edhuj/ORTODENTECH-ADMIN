ea78b0ec17af489010e13f81ee0080c9562a5ab3
EdgarJHJ <edgarjhj@ehuaranga.edhuar>
2019-03-18 14:13:08 -0500

    heatmap

--- resources/views/location/index.blade.php
+++ resources/views/location/index.blade.php
@@ -54,17 +54,7 @@
             {
               featureType: 'poi',
               elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'geometry',
-              stylers: [{color: '#263c3f'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#6b9a76'}]
+              stylers: [{visibility: 'off'}]
             },
             {
               featureType: 'road',
@@ -95,57 +85,6 @@
               featureType: 'road.highway',
               elementType: 'labels.text.fill',
               stylers: [{color: '#f3d19c'}]
-            },
-            {
-              featureType: 'transit',
-              elementType: 'geometry',
-              stylers: [{color: '#2f3948'}]
-            },
-            {
-              featureType: 'transit.station',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'geometry',
-              stylers: [{color: '#17263c'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#515c6d'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.stroke',
-              stylers: [{color: '#17263c'}]
-            },
-            {
-              featureType: 'poi.business',
-              stylers: [{visibility: 'off'}]
-            },
-            {
-              featureType: 'poi.attraction',
-              stylers: [{visibility: 'off'}]
-            },
-            {
-              featureType: 'poi.park',
-              stylers: [{visibility: 'off'}]
-            },
-            {
-              featureType: 'poi.school',
-              stylers: [{visibility: 'off'}]
-            },
-            {
-              featureType: 'transit',
-              elementType: 'labels.icon',
-              stylers: [{visibility: 'off'}]
-            },
-            {
-              featureType: "landscape",
-              elementType: "labels",
-              stylers: [{ visibility: "off" }]
             }
           ]
           });
