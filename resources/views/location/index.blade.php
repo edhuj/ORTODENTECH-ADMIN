eec9a1fe218dfa9c86f03bc79100e5c7229947c5
EdgarJHJ <edgarjhj@ehuaranga.edhuar>
2019-03-18 12:20:57 -0500

    heatmap

--- resources/views/location/index.blade.php
+++ resources/views/location/index.blade.php
@@ -4,7 +4,7 @@
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-      <title>Ortodontech</title>
+      <title>Signum</title>
 
       <!-- Icons -->
       <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
@@ -485,68 +485,9 @@
             <header id="page-header">
                 <!-- Header Content -->
                 <div class="content-header">
-                    <!-- Left Section -->
-                    <div>
-                        <!-- Toggle Sidebar -->
-                        <!-- Layout API, functionality initialized in Template._uiApiLayout()-->
-                        <button type="button" class="btn btn-dual mr-1" data-toggle="layout" data-action="sidebar_toggle">
-                            <i class="fa fa-fw fa-bars"></i>
-                        </button>
-                        <!-- END Toggle Sidebar -->
-
-                        <!-- Open Search Section -->
-                        <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
-                        <button type="button" class="btn btn-dual" data-toggle="layout" data-action="header_search_on">
-                            <i class="fa fa-fw fa-search"></i> <span class="ml-1 d-none d-sm-inline-block">Search</span>
-                        </button>
-                        <!-- END Open Search Section -->
-                    </div>
-                    <!-- END Left Section -->
 
                     <!-- Right Section -->
                     <div>
-                        <!-- User Dropdown -->
-                        <div class="dropdown d-inline-block">
-                            <button type="button" class="btn btn-dual" id="page-header-user-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fa fa-fw fa-user d-sm-none"></i>
-                                <span class="d-none d-sm-inline-block">Admin</span>
-                                <i class="fa fa-fw fa-angle-down ml-1 d-none d-sm-inline-block"></i>
-                            </button>
-                            <div class="dropdown-menu dropdown-menu-right p-0" aria-labelledby="page-header-user-dropdown">
-                                <div class="bg-primary-darker rounded-top font-w600 text-white text-center p-3">
-                                    User Options
-                                </div>
-                                <div class="p-2">
-                                    <a class="dropdown-item" href="be_pages_generic_profile.html">
-                                        <i class="far fa-fw fa-user mr-1"></i> Profile
-                                    </a>
-                                    <a class="dropdown-item d-flex align-items-center justify-content-between" href="be_pages_generic_inbox.html">
-                                        <span><i class="far fa-fw fa-envelope mr-1"></i> Inbox</span>
-                                        <span class="badge badge-primary">3</span>
-                                    </a>
-                                    <a class="dropdown-item" href="be_pages_generic_invoice.html">
-                                        <i class="far fa-fw fa-file-alt mr-1"></i> Invoices
-                                    </a>
-                                    <div role="separator" class="dropdown-divider"></div>
-
-                                    <!-- Toggle Side Overlay -->
-                                    <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
-                                    <a class="dropdown-item" href="javascript:void(0)" data-toggle="layout" data-action="side_overlay_toggle">
-                                        <i class="far fa-fw fa-building mr-1"></i> Settings
-                                    </a>
-                                    <!-- END Side Overlay -->
-
-                                    <div role="separator" class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="op_auth_signin.html">
-                                        <i class="far fa-fw fa-arrow-alt-circle-left mr-1"></i> Sign Out
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- END User Dropdown -->
-
-
-                        <!-- Toggle Side Overlay -->
                         <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
                         <button type="button" class="btn btn-dual" data-toggle="layout" data-action="side_overlay_toggle">
                             <i class="far fa-fw fa-list-alt"></i>
@@ -556,24 +497,6 @@
                     <!-- END Right Section -->
                 </div>
                 <!-- END Header Content -->
-
-                <!-- Header Search -->
-                <div id="page-header-search" class="overlay-header bg-primary">
-                    <div class="content-header">
-                        <form class="w-100" action="be_pages_generic_search.html" method="POST">
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
-                                    <button type="button" class="btn btn-primary" data-toggle="layout" data-action="header_search_off">
-                                        <i class="fa fa-fw fa-times-circle"></i>
-                                    </button>
-                                </div>
-                                <input type="text" class="form-control border-0" placeholder="Search or hit ESC.." id="page-header-search-input" name="page-header-search-input">
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <!-- END Header Search -->
 
                 <!-- Header Loader -->
                 <!-- Please check out the Loaders page under Components category to see examples of showing/hiding it -->
