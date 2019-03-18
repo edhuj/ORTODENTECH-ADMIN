8d0610aefb2f3b5d6d22e0138b85199853a0b6c0
EdgarJHJ <edgarjhj@ehuaranga.edhuar>
2019-03-18 16:33:30 -0500

    heatmap

--- resources/views/location/index.blade.php
+++ resources/views/location/index.blade.php
@@ -157,8 +157,8 @@
                                         <div class="block-content">
                                         @foreach($manufacturers as $manufacturer)
                                             <div class="custom-control custom-checkbox custom-control-primary mb-1">
-                                              <input type="checkbox" class="custom-control-input" id="so-settings-.{{$manufacturer->manufacturer}}" name="so-settings-status" value="1">
-                                              <label class="custom-control-label" for="so-settings-status">{{$manufacturer->manufacturer}}</label>
+                                              <input type="checkbox" class="custom-control-input" id="so-.{{$manufacturer->manufacturer}}-status" name="so-.{{$manufacturer->manufacturer}}-status" value="1">
+                                              <label class="custom-control-label" for="so-.{{$manufacturer->manufacturer}}-status">{{$manufacturer->manufacturer}}</label>
                                             </div>
                                         @endforeach
 
