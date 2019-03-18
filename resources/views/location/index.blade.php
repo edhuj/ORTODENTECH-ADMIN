69512bae53265855af7d34b5644dc9d77bb59e22
EdgarJHJ <edgarjhj@ehuaranga.edhuar>
2019-03-18 10:32:08 -0500

    heatmap

--- resources/views/location/index.blade.php
+++ resources/views/location/index.blade.php
@@ -59,7 +59,7 @@
             center: {
               lat: parseFloat(signalLocations[i].latitude),lng: parseFloat(signalLocations[i].longitude),
             },
-            radius:10
+            radius:parseFloat(signalLocations[i].accuracy)
           });
         }
 
