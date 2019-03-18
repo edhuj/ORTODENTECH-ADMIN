019bbc67ed9b53bb7b7f34a84798d073816fee6b
EdgarJHJ <edgarjhj@ehuaranga.edhuar>
2019-03-18 11:41:09 -0500

    heatmap

--- resources/views/location/index.blade.php
+++ resources/views/location/index.blade.php
@@ -1,38 +1,28 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-        <title>Dashmix - Bootstrap 4 Admin Template &amp; UI Framework</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-        <meta name="description" content="Dashmix - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
-        <meta name="author" content="pixelcave">
-        <meta name="robots" content="noindex, nofollow">
+      <title>Signum</title>
 
-        <!-- Open Graph Meta -->
-        <meta property="og:title" content="Dashmix - Bootstrap 4 Admin Template &amp; UI Framework">
-        <meta property="og:site_name" content="Dashmix">
-        <meta property="og:description" content="Dashmix - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
-        <meta property="og:type" content="website">
-        <meta property="og:url" content="">
-        <meta property="og:image" content="">
+      <!-- Icons -->
+      <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
+      <link rel="shortcut icon" href="media/favicons/favicon.png">
+      <link rel="icon" type="image/png" sizes="192x192" href="media/favicons/favicon-192x192.png">
+      <link rel="apple-touch-icon" sizes="180x180" href="media/favicons/apple-touch-icon-180x180.png">
+      <!-- END Icons -->
 
-        <!-- Icons -->
-        <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
-        <link rel="shortcut icon" href="assets/media/favicons/favicon.png">
-        <link rel="icon" type="image/png" sizes="192x192" href="assets/media/favicons/favicon-192x192.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="assets/media/favicons/apple-touch-icon-180x180.png">
-        <!-- END Icons -->
+      <!-- Stylesheets -->
+      <!-- Fonts and Dashmix framework -->
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700">
+      <link rel="stylesheet" id="css-main" href="/css/dashmix.min.css">
 
-        <!-- Stylesheets -->
-        <!-- Fonts and Dashmix framework -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700">
-        <link rel="stylesheet" id="css-main" href="assets/css/dashmix.min.css">
+      <!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
+      <!-- <link rel="stylesheet" id="css-theme" href="assets/css/themes/xwork.min.css"> -->
+      <!-- END Stylesheets -->
 
-        <!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
-        <!-- <link rel="stylesheet" id="css-theme" href="assets/css/themes/xwork.min.css"> -->
-        <!-- END Stylesheets -->
     </head>
     <body>
         <!-- Page Container -->
@@ -248,7 +238,7 @@
             <!-- Main Container -->
             <main id="main-container">
 
-                
+
                 <footer id="page-footer" class="bg-white">
                     <div class="content py-5">
                         <div class="row font-size-sm">
