dc6d421f9385b12f100bda747e976de48f3e4781
EdgarJHJ <edgarjhj@ehuaranga.edhuar>
2019-03-18 11:39:45 -0500

    heatmap

--- resources/views/location/index.blade.php
+++ resources/views/location/index.blade.php
@@ -1,77 +1,278 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
-    <title>Signum</title>
-    <style>
-      #map {
-        height: 100%;
-      }
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-    </style>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-    <script>
-      function initMap() {
+        <title>Dashmix - Bootstrap 4 Admin Template &amp; UI Framework</title>
 
-        signalLocations = {!!json_encode($locations)!!};
+        <meta name="description" content="Dashmix - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+        <meta name="author" content="pixelcave">
+        <meta name="robots" content="noindex, nofollow">
 
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 11,
-          center: {lat:-12.066910, lng:-77.023073},
-          mapTypeId: 'terrain'
-        });
+        <!-- Open Graph Meta -->
+        <meta property="og:title" content="Dashmix - Bootstrap 4 Admin Template &amp; UI Framework">
+        <meta property="og:site_name" content="Dashmix">
+        <meta property="og:description" content="Dashmix - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="">
+        <meta property="og:image" content="">
 
-        console.log(signalLocations.length);
-        for (i = 0; i < signalLocations.length; i++) {
-          console.log("Hello "+signalLocations[i].accuracy);
-          if(parseInt(signalLocations[i].networkType) == 15 ){ //3G
-              if(parseInt(signalLocations[i].level) == 1){
-                mycolor = '#f03b20'
-              }
-              if(parseInt(signalLocations[i].level) == 2){
-                mycolor = '#feb24c'
-              }
-              if(parseInt(signalLocations[i].level) == 3){
-                mycolor = '#ffeda0'
-              }
-              if(parseInt(signalLocations[i].level) == 4){
-                mycolor = '#31a354'
-              }
-              if(parseInt(signalLocations[i].level) == 5){
-                mycolor = '#31a354'
-              }
-          }
-          else if(parseInt(signalLocations[i].networkType) == 13){//4G
-            mycolor = '#0000ff'
-          }
-          new google.maps.Circle({
-            strokeColor: mycolor,
-            strokeOpacity: 1.0,
-            strokeWeight: 2,
-            fillColor: mycolor,
-            fillOpacity: 1.0,
-            map: map,
-            center: {
-              lat: parseFloat(signalLocations[i].latitude),lng: parseFloat(signalLocations[i].longitude),
-            },
-            radius:20
-          });
-        }
+        <!-- Icons -->
+        <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
+        <link rel="shortcut icon" href="assets/media/favicons/favicon.png">
+        <link rel="icon" type="image/png" sizes="192x192" href="assets/media/favicons/favicon-192x192.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="assets/media/favicons/apple-touch-icon-180x180.png">
+        <!-- END Icons -->
 
-      }
-    </script>
+        <!-- Stylesheets -->
+        <!-- Fonts and Dashmix framework -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700">
+        <link rel="stylesheet" id="css-main" href="assets/css/dashmix.min.css">
 
-  </head>
-  <body>
-    <div id="map"></div>
+        <!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
+        <!-- <link rel="stylesheet" id="css-theme" href="assets/css/themes/xwork.min.css"> -->
+        <!-- END Stylesheets -->
+    </head>
+    <body>
+        <!-- Page Container -->
+        <!--
+            Available classes for #page-container:
 
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCGAOvDgtDnuRnLbShwZpEGVZTRPZNINIQ&callback=initMap">
-    </script>
-  </body>
+        GENERIC
+
+            'enable-cookies'                            Remembers active color theme between pages (when set through color theme helper Template._uiHandleTheme())
+
+        SIDEBAR & SIDE OVERLAY
+
+            'sidebar-r'                                 Right Sidebar and left Side Overlay (default is left Sidebar and right Side Overlay)
+            'sidebar-mini'                              Mini hoverable Sidebar (screen width > 991px)
+            'sidebar-o'                                 Visible Sidebar by default (screen width > 991px)
+            'sidebar-o-xs'                              Visible Sidebar by default (screen width < 992px)
+            'sidebar-dark'                              Dark themed sidebar
+
+            'side-overlay-hover'                        Hoverable Side Overlay (screen width > 991px)
+            'side-overlay-o'                            Visible Side Overlay by default
+
+            'enable-page-overlay'                       Enables a visible clickable Page Overlay (closes Side Overlay on click) when Side Overlay opens
+
+            'side-scroll'                               Enables custom scrolling on Sidebar and Side Overlay instead of native scrolling (screen width > 991px)
+
+        HEADER
+
+            ''                                          Static Header if no class is added
+            'page-header-fixed'                         Fixed Header
+
+
+        Footer
+
+            ''                                          Static Footer if no class is added
+            'page-footer-fixed'                         Fixed Footer (please have in mind that the footer has a specific height when is fixed)
+
+        HEADER STYLE
+
+            ''                                          Classic Header style if no class is added
+            'page-header-dark'                          Dark themed Header
+            'page-header-glass'                         Light themed Header with transparency by default
+                                                        (absolute position, perfect for light images underneath - solid light background on scroll if the Header is also set as fixed)
+            'page-header-glass page-header-dark'         Dark themed Header with transparency by default
+                                                        (absolute position, perfect for dark images underneath - solid dark background on scroll if the Header is also set as fixed)
+
+        MAIN CONTENT LAYOUT
+
+            ''                                          Full width Main Content if no class is added
+            'main-content-boxed'                        Full width Main Content with a specific maximum width (screen width > 1200px)
+            'main-content-narrow'                       Full width Main Content with a percentage width (screen width > 1200px)
+        -->
+        <div id="page-container" class="page-header-fixed page-header-glass main-content-boxed">
+
+            <!-- Header -->
+            <header id="page-header" class="invisible" data-toggle="appear" data-class="animated fadeInDown" data-timeout="600">
+                <!-- Header Content -->
+                <div class="content-header">
+                    <!-- Left Section -->
+                    <div class="d-flex align-items-center">
+                        <!-- Logo -->
+                        <a class="link-fx font-size-lg font-w700 text-dark" href="index.html">
+                            Dash<span class="text-primary">mix</span> <small class="font-w600 text-black">1.5</small>
+                        </a>
+                        <!-- END Logo -->
+                    </div>
+                    <!-- END Left Section -->
+
+                    <!-- Center Section -->
+                    <div class="d-none d-lg-flex align-items-center">
+                        <!-- Menu -->
+                        <ul class="nav-main nav-main-horizontal nav-main-hover">
+                            <li class="nav-main-item">
+                                <a class="nav-main-link" data-toggle="scroll-to" data-speed="750" href="#dm-dashboards">
+                                    <i class="nav-main-link-icon fa fa-compass"></i>
+                                    <span class="nav-main-link-name">Dashboards</span>
+                                </a>
+                            </li>
+                            <li class="nav-main-item">
+                                <a class="nav-main-link" data-toggle="scroll-to" data-speed="750" href="#dm-widgets">
+                                    <i class="nav-main-link-icon fa fa-puzzle-piece"></i>
+                                    <span class="nav-main-link-name">Widgets</span>
+                                </a>
+                            </li>
+                            <li class="nav-main-item">
+                                <a class="nav-main-link" data-toggle="scroll-to" data-speed="750" href="#dm-layout">
+                                    <i class="nav-main-link-icon fa fa-fire"></i>
+                                    <span class="nav-main-link-name">Layout</span>
+                                </a>
+                            </li>
+                            <li class="nav-main-item">
+                                <a class="nav-main-link" data-toggle="scroll-to" data-speed="750" href="#dm-toolkit">
+                                    <i class="nav-main-link-icon fab fa-node-js"></i>
+                                    <span class="nav-main-link-name">Toolkit</span>
+                                </a>
+                            </li>
+                            <li class="nav-main-item">
+                                <a class="nav-main-link" data-toggle="scroll-to" data-speed="750" href="#dm-features">
+                                    <i class="nav-main-link-icon fa fa-heartbeat"></i>
+                                    <span class="nav-main-link-name">Features</span>
+                                </a>
+                            </li>
+                            <li class="nav-main-item">
+                                <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true" aria-expanded="false" href="#">
+                                    <i class="nav-main-link-icon fa fa-brush"></i>
+                                    <span class="nav-main-link-name">Themes</span>
+                                </a>
+                                <ul class="nav-main-submenu">
+                                    <li class="nav-main-item">
+                                        <a class="nav-main-link" data-toggle="theme" data-theme="default" href="#">
+                                            <i class="nav-main-link-icon fa fa-circle text-default"></i>
+                                            <span class="nav-main-link-name">Default</span>
+                                        </a>
+                                    </li>
+                                    <li class="nav-main-item">
+                                        <a class="nav-main-link" data-toggle="theme" data-theme="assets/css/themes/xwork.min.css" href="#">
+                                            <i class="nav-main-link-icon fa fa-circle text-xwork"></i>
+                                            <span class="nav-main-link-name">xWork</span>
+                                        </a>
+                                    </li>
+                                    <li class="nav-main-item">
+                                        <a class="nav-main-link" data-toggle="theme" data-theme="assets/css/themes/xmodern.min.css" href="#">
+                                            <i class="nav-main-link-icon fa fa-circle text-xmodern"></i>
+                                            <span class="nav-main-link-name">xModern</span>
+                                        </a>
+                                    </li>
+                                    <li class="nav-main-item">
+                                        <a class="nav-main-link" data-toggle="theme" data-theme="assets/css/themes/xeco.min.css" href="#">
+                                            <i class="nav-main-link-icon fa fa-circle text-xeco"></i>
+                                            <span class="nav-main-link-name">xEco</span>
+                                        </a>
+                                    </li>
+                                    <li class="nav-main-item">
+                                        <a class="nav-main-link" data-toggle="theme" data-theme="assets/css/themes/xsmooth.min.css" href="#">
+                                            <i class="nav-main-link-icon fa fa-circle text-xsmooth"></i>
+                                            <span class="nav-main-link-name">xSmooth</span>
+                                        </a>
+                                    </li>
+                                    <li class="nav-main-item">
+                                        <a class="nav-main-link" data-toggle="theme" data-theme="assets/css/themes/xinspire.min.css" href="#">
+                                            <i class="nav-main-link-icon fa fa-circle text-xinspire"></i>
+                                            <span class="nav-main-link-name">xInspire</span>
+                                        </a>
+                                    </li>
+                                    <li class="nav-main-item">
+                                        <a class="nav-main-link" data-toggle="theme" data-theme="assets/css/themes/xdream.min.css" href="#">
+                                            <i class="nav-main-link-icon fa fa-circle text-xdream"></i>
+                                            <span class="nav-main-link-name">xDream</span>
+                                        </a>
+                                    </li>
+                                    <li class="nav-main-item">
+                                        <a class="nav-main-link" data-toggle="theme" data-theme="assets/css/themes/xpro.min.css" href="#">
+                                            <i class="nav-main-link-icon fa fa-circle text-xpro"></i>
+                                            <span class="nav-main-link-name">xPro</span>
+                                        </a>
+                                    </li>
+                                    <li class="nav-main-item">
+                                        <a class="nav-main-link" data-toggle="theme" data-theme="assets/css/themes/xplay.min.css" href="#">
+                                            <i class="nav-main-link-icon fa fa-circle text-xplay"></i>
+                                            <span class="nav-main-link-name">xPlay</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <!-- END Menu -->
+                    </div>
+                    <!-- END Center Section -->
+
+                    <!-- Right Section -->
+                    <div class="d-flex align-items-center">
+                        <!-- Call to action -->
+                        <a class="btn btn-outline-primary" href="https://1.envato.market/r6y">
+                            <i class="fa fa-fw fa-shopping-bag mr-1 opacity-75"></i>
+                            <span class="font-w700 font-size-sm text-uppercase">Buy Now</span>
+                        </a>
+                        <!-- END Call to action -->
+                    </div>
+                    <!-- END Right Section -->
+                </div>
+                <!-- END Header Content -->
+
+                <!-- Header Search -->
+                <div id="page-header-search" class="overlay-header bg-primary">
+                    <div class="content-header">
+                        <form class="w-100" action="be_pages_generic_search.html" method="POST">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+                                    <button type="button" class="btn btn-primary" data-toggle="layout" data-action="header_search_off">
+                                        <i class="fa fa-fw fa-times-circle"></i>
+                                    </button>
+                                </div>
+                                <input type="text" class="form-control border-0" placeholder="Search or hit ESC.." id="page-header-search-input" name="page-header-search-input">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- END Header Search -->
+
+                <!-- Header Loader -->
+                <!-- Please check out the Loaders page under Components category to see examples of showing/hiding it -->
+                <div id="page-header-loader" class="overlay-header bg-primary-darker">
+                    <div class="content-header">
+                        <div class="w-100 text-center">
+                            <i class="fa fa-fw fa-2x fa-sun fa-spin text-white"></i>
+                        </div>
+                    </div>
+                </div>
+                <!-- END Header Loader -->
+            </header>
+            <!-- END Header -->
+
+            <!-- Main Container -->
+            <main id="main-container">
+
+                
+                <footer id="page-footer" class="bg-white">
+                    <div class="content py-5">
+                        <div class="row font-size-sm">
+                            <div class="col-sm-6 order-sm-2 py-1 text-center text-sm-right">
+                                Crafted with <i class="fa fa-heart text-danger"></i> by <a class="font-w600" href="https://1.envato.market/ydb" target="_blank">pixelcave</a>
+                            </div>
+                            <div class="col-sm-6 order-sm-1 py-1 text-center text-sm-left">
+                                <a class="font-w600" href="https://1.envato.market/r6y" target="_blank">Dashmix 1.5</a> &copy; <span data-toggle="year-copy">2018</span>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+                <!-- END Footer -->
+
+            </main>
+            <!-- END Main Container -->
+        </div>
+        <!-- END Page Container -->
+
+
+        <script src="/js/dashmix.core.min.js"></script>
+        <script src="/js/dashmix.app.min.js"></script>
+        <script src="/js/plugins/jquery-sparkline/jquery.sparkline.min.js"></script>
+
+        <!-- Page JS Helpers (jQuery Sparkline Plugin) -->
+    </body>
 </html>
