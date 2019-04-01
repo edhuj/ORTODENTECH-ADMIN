f56ad86a2aec334bf834f4ef2a7cb5ef4183fac7
EdgarJHJ <edgarjhj@ehuaranga.edhuar>
2019-04-01 04:43:24 -0500

    alo

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -239,13 +239,17 @@ class ApiController extends Controller
     public function getMatrix(){
       $locations = Location::first();
       foreach ($locations as $location) {
-        $users = DB::table('locations')
+        /*$users = DB::table('locations')
                     ->select('id')
                     ->select('( 3959 * acos( cos( radians(37) ) * cos( radians( CAST($location->latitude AS float) ) ) * cos( radians( CAST($location->longitude AS FLOAT) ) - radians(-122) ) + sin( radians(37) ) * sin( radians( CAST($location->latitude) AS FLOAT ) ) ) ) AS distance')
                     ->having('distance', '<1000')
                     ->orderBy('distance', 'desc')
-                    ->get();
-        echo($users);
+                    ->get();*/
+
+        $sqlQuery = "SELECT id, ( 3959 * acos( cos( radians(37) ) * cos( radians( CAST($location->latitude AS float) ) ) * cos( radians( CAST($location->longitude AS FLOAT) ) - radians(-122) ) + sin( radians(37) ) * sin( radians( CAST($location->latitude) AS FLOAT ) ) ) ) AS distance FROM hexagons HAVING distance < 25 ORDER BY distance LIMIT 0 , 1";
+        $result = DB::select(DB::raw($sqlQuery));
+
+        echo($result);
       }
     }
 }
