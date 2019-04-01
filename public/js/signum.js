d9cf61196bb1f931d9bc972629e0072998b8d928
EdgarJHJ <edgarjhj@ehuaranga.edhuar>
2019-04-01 15:15:03 -0500

    hexas

--- public/js/signum.js
+++ public/js/signum.js
@@ -101,10 +101,10 @@ function drawHexagon(map, position, radius, fillColor){
 			// Construct the polygon.
 			var polygon = new google.maps.Polygon({
 					paths: coordinates,
-					strokeColor: '#FF0000',
+					strokeColor: '#FFFFFF',
 					strokeOpacity: 0.8,
 					strokeWeight: 2,
-					fillColor: '#FF0000',
+					fillColor: fillColor,
 					fillOpacity: 0.35
 			});
 			polygon.setMap(map);
