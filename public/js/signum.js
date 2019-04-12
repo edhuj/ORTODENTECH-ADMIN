0e0e6d5b834832dee5201efd8b8deda54312f6f8
EdgarJHJ <edgarjhj@ehuaranga.edhuar>
2019-04-12 13:38:22 -0500

    hexX

--- public/js/signum.js
+++ public/js/signum.js
@@ -161,7 +161,7 @@ function drawHexagon(map, position, radius, fillColor, indexID){
 			console.log(level*hexagonRadio*Math.sqrt(3));
 			for(var position=0; position<360; position+=60){
 				currentPosition = google.maps.geometry.spherical.computeOffset(hexagonCenter, level*hexagonRadio*Math.sqrt(3), position);
-				drawRawHexagons(window.map, currentPosition, hexagonRadio, "#ffff00", signalLocations["hexagon"].id, 120+position, level);
+				drawRawHexagonsX(window.map, currentPosition, hexagonRadio, "#ffff00", signalLocations["hexagon"].id, 120+position, level);
 			}
 			level = level+1;
 		}
