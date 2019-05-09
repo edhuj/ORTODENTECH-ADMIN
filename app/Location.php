02449ac5f4f7b1c4f98dbc4e482ef18cdaa42fb5
EdgarJHJ <edgarjhj@ehuaranga.edhuar>
2019-05-09 14:42:59 -0500

    assign

--- app/Location.php
+++ app/Location.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
     //
+    function getClosestHexagon(){
+      return DB::select('call assignHexagon2(?)', this->id);
+    }
 }
