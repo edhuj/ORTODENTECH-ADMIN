04fdb2936018ffff93bf56faf053baffd5812a05
edhu <edgar.huaranga@uni.pe>
2021-04-06 01:01:33 -0500

    groupedby cat

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -33,7 +33,7 @@ class ApiController extends Controller
 
 
     public function topics(){
-      $questionsGroupedByTopic = Question::all()->groupBy(['category']);
+      $questionsGroupedByTopic = Question::all()->groupBy('category');
 
       return response()->json([
         'requestType' => request('requestType'),
